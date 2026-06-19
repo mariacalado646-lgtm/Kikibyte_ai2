20d2190cad346259c166fc2f48c6344af9259feb
MrTimnn <pv23009@alunos.estgv.ipv.pt>
2026-06-19 19:28:58 +0100

    fix: update express 5->4 and bcrypt->bcryptjs for Render compatibility

--- kikibyte-api/config/database.js
+++ kikibyte-api/config/database.js
@@ -4,5 +4,16 @@ dotenv.config()
 
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    logging: false
+    logging: false,
+    dialectOptions: {
+        ssl: process.env.DATABASE_SSL !== 'false'
+            ? { require: true, rejectUnauthorized: false }
+            : false
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 30000,
+        acquire: 60000
+    }
 })
--- kikibyte-api/controllers/authController.js
+++ kikibyte-api/controllers/authController.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { Utilizador } from '../models/Utilizador.js'
 import { Op } from 'sequelize'
